938cab43b3b09a3b3f57e595c956c31b62c8c532
melgendy <mohamed.elgendy@blacklane.com>
2018-09-24 17:41:06 +0200

    fix python path

--- build-report.sh
+++ build-report.sh
@@ -8,7 +8,7 @@ echo "Copy unit tests report"
 cp app/build/reports/tests/testDebugUnitTest/index.html "$REPORT_PATH/unittests.html"
 
 echo "Fetching build_report.py"
-curl https://raw.githubusercontent.com/blacklane/zulu-scripts/master/buildreport/build_report.py -o "$REPORT_PATH/build_report.py"
+curl https://raw.githubusercontent.com/elgendyLanes/BuildReports/master/build_report.py -o "$REPORT_PATH/build_report.py"
 
 echo "Building report"
 python "$REPORT_PATH/build_report.py" --report=$REPORT_PATH
