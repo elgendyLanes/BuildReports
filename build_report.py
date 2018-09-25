b4119cf1bd30a729a34048f3446ae562006f64a5
melgendy <mohamed.elgendy@blacklane.com>
2018-09-25 18:09:21 +0200

    adding parsino

--- build_report.py
+++ build_report.py
@@ -113,25 +113,15 @@ def generate_unit_tests():
 
 
 def generate_coverage_report():
-  soup = get_soup(REPORT_PATH + "/unittests.html")
-
-  # Add summary
-  div_summary = soup.find('div', attrs={'id': 'summary'})
-  write(str(div_summary))
-
-  # Tabs are generated dynamically based on the status. For example: If there is no failure test
-  # Failure tab won't be shown, therefore we need to dynamically fetch the classes tab which is
-  # always the last tab
-  tab_links = soup.find('ul', attrs={'class': 'tabLinks'}).find_all('li')
-  last_tab = "tab" + str(len(tab_links) - 1)
+  soup = get_soup(REPORT_PATH + "/coveragetests.html")
 
   # Add test classes
-  div_classes = soup.find('div', attrs={'id': last_tab})
-  div_classes.h2.extract()
+  div_table = soup.find('table', attrs={'id': 'coveragetable'})
+  div_table.h2.extract()
 
-  tr_tags = div_classes.tbody.find_all('tr')
-  for tr in tr_tags:
-    td_tags = tr.find_all('td')
+  body_tr_tags = div_table.tbody.find_all('tr')
+  for tr in body_tr_tags:
+    body_td_tags = tr.find_all('td')
 
     # 0 : Empty column, we will replace it with name
     # 1 : Test count
@@ -140,39 +130,39 @@ def generate_coverage_report():
     # 4 : Duration in second
     # 5 : Status
 
-    for index, item in enumerate(td_tags):
+    for index, item in enumerate(body_td_tags):
       # There is a bug in the generated report. An empty redundant column is generated.
       # We fix it by removing basically and add the class name as new column
       if index == 0:
         item.extract()
 
       # If the test class has failures, paint to red
-      if index == 2:
+      if index == 5:
         fail_count = float(item.string)
         if fail_count > 0:
           tr['bgcolor'] = "#ff9999"
 
       # If the test class has ignoring tests, paint to orange
-      if index == 3:
+      if index == 6:
         ignore_count = float(item.string)
         if ignore_count > 0:
           tr['bgcolor'] = "#ffeb99"
 
       # If the duration is longer than 1 second, paint to blue
-      if index == 4:
+      if index == 7:
         duration = float(item.string[:-1])
         if duration > 1:
           tr['bgcolor'] = "#ccccff"
 
   # There is a bug in generated test report html which is class names are not inside <td> tag
   # Remove the links and move them inside a td tag
-  links = div_classes.find_all('a')
+  links = div_table.find_all('a')
   for a in links:
     a.wrap(soup.new_tag('td'))
     a.insert_after(str(a.string))
     a.extract()
 
-  write(str(div_classes))
+  write(str(div_table))
 
 
 if __name__ == '__main__':
@@ -190,7 +180,7 @@ with open(REPORT_PATH + '/build-report.html', 'w+') as file:
   #jacoco-part
   print "Generating coverage test report"
   add_header("Coverage Report")
-  #generate_coverage_report()
+  generate_coverage_report()
 
   print "Build report is generated at " + REPORT_PATH + "/build-report.html"
   write('</body></html>')
